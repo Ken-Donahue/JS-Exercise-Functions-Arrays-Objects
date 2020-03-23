502914cc86de45097b47266125d3aeb6008742b8
Ken Donahue <ken.w.donahue@gmail.com>
2020-03-23 17:54:39 -0400

    Passed a few tests, starting on appleIndex next

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return "Goodbye, " + name + ". Have a great day."
 }
 
 /**
@@ -53,8 +53,8 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
-  /* code here */
+function temperatureCtoF(celsius) {
+  return Math.round(celsius*(9/5)+32);
 }
 
 /**
@@ -74,8 +74,13 @@ function temperatureCtoF(/* code here */) {
  * 
  * Hint: You can call your `temperatureCtoF` function from inside `temperatureInF`.
 */
-function temperatureInF(/* code here */) {
-  /* code here */
+function temperatureInF(Temp,unitOfMeasure) {
+  if (unitOfMeasure === "C"){
+    return temperatureCtoF(Temp)+"F";
+  }
+  else{
+    return Math.round(Temp)+"F";
+  }
 }
 
 
@@ -95,10 +100,9 @@ function temperatureInF(/* code here */) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
-}
-
+function makePersonObject(id, name, email) {
+  return {id, name,email};
+};
 /**
  * ### Challenge `getName`
  * 
@@ -112,9 +116,9 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+function getName(makePersonObject) {
+  return "Hello, my name is " + name;
+};
 
 
 /**
@@ -132,8 +136,9 @@ function getName(/* code here */) {
  * passing in [ 'orange', 'grape', 'apple', 'banana', 'mango' ] as the argument,
  * the returned value should be: 2.
 */
-function appleIndex(/* code here */) {
-  /* code here */
+function appleIndex(fruit) {
+ return indexOf(Array["apple"]);
+
 }
 
 /**
